ce1dc66d869622311d8c37b237946cf9bd3a8889
Marcos Mathias <marcos-github@memathias.com>
2020-07-12 20:48:10 -0300

    Initializing Product Create Tests

---
+++ backend/src/__tests__/integration/products/products.create.test.ts
@@ -0,0 +1,20 @@
+// import request from 'supertest';
+// import { isUuid } from 'uuidv4';
+// import { parseISO, isToday } from 'date-fns';
+// import app from '../../../app';
+
+describe('Products Routes', () => {
+  interface Product {
+    name: string;
+    description: string;
+    ownerId: string;
+    price: number;
+    priceOld: number;
+  }
+
+  // Add a user and get its id.
+  // Add two products for this user
+  // Try add another product with the same name
+  // Try add a product with price_old higher than price
+  // List all products from the user
+});
